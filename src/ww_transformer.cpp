1958529b47461bc4401a3ca6e29aa624160bfc7c
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2017-01-20 00:06:18 +0100

    Complete TF construction

--- src/ww_transformer.cpp
+++ src/ww_transformer.cpp
@@ -2,6 +2,7 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Pose.h>
 #include <rosgraph_msgs/Clock.h>
+#include <sensor_msgs/PointCloud2.h>
 
 std::string turtle_name;
 
@@ -12,86 +13,80 @@ geometry_msgs::Pose pose;
 tf::tfMessage transforms;
 ros::Publisher tfPub;
 
-bool tfKnown = false;
 bool tfComplete = false;
-int tfSeq = 0;
-int poseSeq = 0;
+int numJumps = 0;
+int numPoses = 0;
 int numTrf = 0;
 
-void clockCallback(const rosgraph_msgs::Clock::ConstPtr& msg)
+void publish()
 {
-	lastClkTime = clkTime;
-	clkTime = msg->clock;
-	if (clkTime < lastClkTime || clkTime.toSec()-lastClkTime.toSec() > 1.0){
-		ROS_INFO("Time jump detected %lf",msg->clock.toSec());
-		firstTime = clkTime;
-		if (tfTime < clkTime){
-			ROS_INFO("TF tree %i is obsolete.",numTrf);
-		       	tfKnown = false;
-		}
-	}
-	if (tfKnown && tfComplete == false && tfSeq == poseSeq){
-		int i = 0;
-		while (i<transforms.transforms.size())
-		{
-			if (transforms.transforms[i].header.frame_id == "/head_frame")transforms.transforms.erase(transforms.transforms.begin()+i);
-			else if (transforms.transforms[i].header.frame_id.find("chest_xtion")!= std::string::npos) transforms.transforms.erase(transforms.transforms.begin()+i);
-			else i++;
-		}
+	if (numJumps == numPoses && tfComplete == false){
+
 		tf::Transform transform;
+		transforms.transforms.clear();
 		geometry_msgs::TransformStamped stampedTransform;
 		transform.setOrigin(tf::Vector3(pose.position.x, pose.position.y, pose.position.z) );
 		transform.setRotation(tf::Quaternion(pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w));
-		transformStampedTFToMsg(tf::StampedTransform(transform, tfTime, "map", "base_footprint"),stampedTransform);
+		transformStampedTFToMsg(tf::StampedTransform(transform,firstTime, "map", "base_footprint"),stampedTransform);
 		transforms.transforms.push_back(stampedTransform);
 
-		transform.setOrigin(tf::Vector3(0,0,0.04572));
-		transform.setRotation(tf::Quaternion(0.706825181105,0,0,0.707388269167));
-		transformStampedTFToMsg(tf::StampedTransform(transform, tfTime,"ptu_pan_motor","ptu_tilt_motor"),stampedTransform);
+		transform.setOrigin(tf::Vector3(0.07,0,0.386));
+		transform.setRotation(tf::Quaternion(0,0,0,1.0));
+		transformStampedTFToMsg(tf::StampedTransform(transform,firstTime,"base_footprint","base_laser_link"),stampedTransform);
 		transforms.transforms.push_back(stampedTransform);
 
-		transform.setOrigin(tf::Vector3(0,0.04572,0));
-		transform.setRotation(tf::Quaternion(-0.706825181105,0,0,0.707388269167));
-		transformStampedTFToMsg(tf::StampedTransform(transform, tfTime,"ptu_tilt_motor","ptu_hinge"),stampedTransform);
+		transform.setOrigin(tf::Vector3(-0.06,0.0450364,1.69986));
+		transform.setRotation(tf::Quaternion(0.5,-0.5,0.5,-0.5));
+		transformStampedTFToMsg(tf::StampedTransform(transform,firstTime,"base_footprint","head_xtion_depth_optical_frame"),stampedTransform);
 		transforms.transforms.push_back(stampedTransform);
-		for (int i = 0;i<transforms.transforms.size();i++) transforms.transforms[i].header.stamp = firstTime;
+
+		transform.setOrigin(tf::Vector3(-0.06,0.0200364,1.69986));
+		transform.setRotation(tf::Quaternion(0.5,-0.5,0.5,-0.5));
+		transformStampedTFToMsg(tf::StampedTransform(transform,firstTime,"base_footprint","head_xtion_rgb_optical_frame"),stampedTransform);
+		transforms.transforms.push_back(stampedTransform);
+
+		for (int i = 0;i<transforms.transforms.size();i++) transforms.transforms[i].header.stamp = firstTime-ros::Duration(1.0);
 		tfPub.publish(transforms);
 		tfComplete = true;
 		numTrf++;
 		ROS_INFO("TF tree %i of %i transforms rebuild, and published with timestamp %lf.",numTrf,(int)transforms.transforms.size(),transforms.transforms[0].header.stamp.toSec());
 	}
-	if (tfKnown && tfComplete)
+	if (numJumps == numPoses && tfComplete)
 	{
-		for (int i = 0;i<transforms.transforms.size();i++) transforms.transforms[i].header.stamp = clkTime;
+		for (int i = 0;i<transforms.transforms.size();i++) transforms.transforms[i].header.stamp = clkTime+ros::Duration(1.0);
 		ROS_INFO("TF tree %i of %i transforms published with timestamp %lf.",numTrf,(int)transforms.transforms.size(),transforms.transforms[0].header.stamp.toSec());
 		tfPub.publish(transforms);
 	}
 }
 
-void tfCallback(const tf::tfMessage::ConstPtr& msg)
+void clockCallback(const rosgraph_msgs::Clock::ConstPtr& msg)
 {
-	ROS_INFO("New TF of %d transforms received with timestamp %lf",(int)msg->transforms.size(),msg->transforms[0].header.stamp.toSec());
-	tfTime = msg->transforms[0].header.stamp;
-	transforms = *msg;
-	tfComplete = false;
-	tfKnown = true;
-	tfSeq++;
+	lastClkTime = clkTime;
+	clkTime = msg->clock;
+	ROS_INFO("Time received %lf",msg->clock.toSec());
+	if (clkTime < lastClkTime || clkTime.toSec()-lastClkTime.toSec() > 1.0){
+		ROS_INFO("Time jump detected %lf",msg->clock.toSec());
+		firstTime = clkTime;
+		numJumps++;
+	}
+	publish();
 }
 
 void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
 {
-	ROS_INFO("New pose received: %lf %lf",msg->position.x,msg->position.y);
+	ROS_INFO("New pose received: %lf %lf %lf",msg->position.x,msg->position.y,ros::Time::now().toSec());
 	pose = *msg;
-	poseSeq++;
+	numPoses++;
+	
+	tfComplete = false;
+	publish();
 }
-
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "ww_transformer");
 	ros::NodeHandle node;
 	clkTime = lastClkTime = ros::Time(0);
 	ros::Subscriber subClk = node.subscribe("/clock", 10, &clockCallback);
-	ros::Subscriber subTf = node.subscribe("/tf_old", 10, &tfCallback);
 	ros::Subscriber subPose = node.subscribe("/robot_pose", 10, &poseCallback);
 	tfPub = node.advertise<tf::tfMessage>("tf", 1);
 	ros::spin();
